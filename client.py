bbb23c08145dde79c4f1fa87223046944cc0a8df
Beth Johanssen <m.charikova@innopolis.ru>
2019-11-27 14:28:22 +0300

    Add client's structure

---
+++ client.py
@@ -0,0 +1,46 @@
+class Client:
+    def __init__(self):
+        return
+
+    def connect_to_server(self, ):
+        return
+
+    def initialize(self):
+        return
+
+    def file_create(self, filepath):
+        return
+
+    def file_read(self, filepath):
+        return
+
+    def file_write(self, filepath):
+        return
+
+    def file_delete(self, filepath):
+        return
+
+    def file_info(self, filepath):
+        return
+
+    def file_copy(self, filepath, dest_filepath):
+        return
+
+    def file_move(self, filepath, dest_filepath):
+        return
+
+    def open_directory(self, filepath):
+        return
+
+    def read_directory(self, filepath):
+        return
+
+    def make_directory(self, filepath):
+        return
+
+    def delete_directory(self, filepath):
+        return
+
+    def client_run(self):
+        return
+    
