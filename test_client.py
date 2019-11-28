b3c9ab74e6b9808c074ee0f70ce08f4307c0b2dc
Roman Solovev <r.solovev@innopolis.ru>
2019-11-28 14:59:10 +0300

    +working download client/server

--- test_client.py
+++ test_client.py
@@ -1,32 +1,30 @@
-import json
-import os
 import socket  # Import socket module
-import time
 
 import requests
 
 s = socket.socket()  # Create a socket object
-host = "10.241.1.230"  # Ip address that the TCPServer  is there
+host = "10.1.1.141"  # Ip address that the TCPServer  is there
 port = 7331  # Reserve a port for your service every new transfer wants a new port or you must wait.
 
-try:
-    requests.get('http://10.241.1.230:1337', json={"command": "file_download",
+
+command = {"command": "file_download",
                                                                      "args": {
                                                                          "username": "roma",
-                                                                         "path": "/privet/alo.txt"
+                                                                         "path": "/regex.pdf"
                                                                      }
-                                                                     }, timeout=0.000001)
+                                                                     }
+try:
+    requests.get('http://10.1.1.141:1337', json=command, timeout=0.000001)
 except requests.exceptions.ReadTimeout:
     pass
 
 
 s.connect((host, port))
-
-with open('received_file', 'wb') as f:
+filename = command["args"]["path"].split("/")[-1]
+with open('{}'.format(filename), 'wb') as f:
     while True:
         print('receiving data...')
         data = s.recv(1024)
-        print('data=%s', (data))
         if not data:
             break
         # write data to a file
